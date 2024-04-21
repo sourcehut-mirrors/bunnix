3ee89d5aaacb34f17ae1f797dc00c4536f5d7123
Drew DeVault <sir@cmpwn.com>
2024-04-21 16:18:30 +0200

    all: import & adapt logging system from Helios

--- arch/x86_64/native.s
+++ arch/x86_64/native.s
@@ -20,3 +20,35 @@ arch.x86_64.outb:
 	mov %sil, %al
 	outb %al, %dx
 	ret
+
+.global arch.x86_64.inb
+arch.x86_64.inb:
+	mov %di, %dx
+	inb %dx, %al
+	ret
+
+.global arch.x86_64.outw
+arch.x86_64.outw:
+	mov %di, %dx
+	mov %si, %ax
+	outw %ax, %dx
+	ret
+
+.global arch.x86_64.inw
+arch.x86_64.inw:
+	mov %di, %dx
+	inw %dx, %ax
+	ret
+
+.global arch.x86_64.outl
+arch.x86_64.outl:
+	mov %di, %dx
+	mov %esi, %eax
+	outl %eax, %dx
+	ret
+
+.global arch.x86_64.inl
+arch.x86_64.inl:
+	mov %di, %dx
+	inl %dx, %eax
+	ret
