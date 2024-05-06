7cab6e3e7729ead86590656cee327c433adea651
Drew DeVault <sir@cmpwn.com>
2024-05-06 22:34:24 +0200

    lib/libc: add wait, waitpid

--- bin/test-c/main.c
+++ bin/test-c/main.c
@@ -1,7 +1,9 @@
 #define _POSIX_C_SOURCE
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
@@ -12,33 +14,26 @@ int main(int argc, char *argv[]) {
 		printf("argc[%d]: %s\n", i, argv[i]);
 	}
 
-	printf("Open /usr/include/stdio.h\n");
-	FILE *f = fopen("/usr/include/stdio.h", "r");
-	if (f == NULL) {
-		perror("fopen");
+	printf("[orig] fork\n");
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("fork");
 		return 1;
-	};
+	} else if (pid == 0) {
+		printf("[child] execl /bin/echo Hello world!\n");
+		execl("/bin/echo", "/bin/echo", "Hello world!", NULL);
+		return 0;
+	}
 
-	int fd = fileno(f);
-	printf("FILE %p w/fd %d\n", (void *)f, fd);
+	printf("[parent] child has pid %d\n", pid);
+	printf("[parent] wait()\n");
 
-	struct stat st;
-	int r = fstat(fd, &st);
-	if (r != 0) {
-		perror("fstat");
-	};
-	printf("fstat: mode %o owner %d/%d\n", st.st_mode, st.st_uid, st.st_gid);
+	int status;
+	waitpid(pid, &status, 0);
 
-	char buf[256];
-	size_t n = fread(buf, 1, sizeof(buf) - 1, f);
-	printf("fread got %zu bytes\n", n);
-	buf[n] = 0;
-	printf("First 256 bytes: %s\n", buf);
-
-	fclose(f);
-
-	printf("execl /bin/echo Hello world!\n");
-	execl("/bin/echo", "/bin/echo", "Hello world!", NULL);
+	assert(WIFEXITED(status));
+	printf("[parent] child exited with status code %d\n",
+			WEXITSTATUS(status));
 
 	return 0;
 }
--- lib/libc/src/process/_Fork.c
+++ lib/libc/src/process/_Fork.c
@@ -5,7 +5,6 @@
 
 pid_t _Fork(void)
 {
-	pid_t ret;
-	ret = __syscall(SYS_fork);
+	pid_t ret = __syscall(SYS_fork);
 	return __syscall_ret(ret);
 }
