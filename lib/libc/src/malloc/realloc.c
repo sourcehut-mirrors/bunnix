1a2c9a17d60c31119d2331b624e5c407efd3dddb
Drew DeVault <sir@cmpwn.com>
2024-05-12 15:40:34 +0200

    libc: fix realloc copy amount

--- lib/libc/src/malloc/realloc.c
+++ lib/libc/src/malloc/realloc.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
+#include "malloc_impl.h"
 
 void *realloc(void *ptr, size_t size) {
 	void *new = malloc(size);
 	if (!new || !ptr) {
 		return new;
 	}
-	memcpy(new, ptr, size);
+	size_t oldsz = (size_t)(((size_t *)ptr)[-1]);
+	memcpy(new, ptr, oldsz);
 	free(ptr);
 	return new;
 }
