54812e98434f4026029d63091b0a6c02df7b1277
Drew DeVault <sir@cmpwn.com>
2024-05-12 12:53:59 +0200

    Flesh out tty implementation, add getty(8)

--- lib/libc/src/unistd/isatty.c
+++ lib/libc/src/unistd/isatty.c
@@ -1,9 +1,18 @@
-#include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "syscall.h"
 
 int isatty(int fd)
 {
-	// TODO Bunnix
+	// XXX: This is less than ideal
+	struct stat st;
+	int ret = fstat(fd, &st);
+	if (ret < 0) {
+		return 0;
+	}
+	if (st.st_rdev >> 32 == 2) {
+		return 1;
+	}
 	return 0;
 }
