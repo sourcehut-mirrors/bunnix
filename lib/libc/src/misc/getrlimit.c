72ee93d8831ffd10525e5411726b05df7b4576be
Drew DeVault <sir@cmpwn.com>
2024-05-14 00:12:00 +0200

    lib/libc: rig up rlimit shim

--- lib/libc/src/misc/getrlimit.c
+++ lib/libc/src/misc/getrlimit.c
@@ -1,9 +1,14 @@
 #include <sys/resource.h>
 #include <errno.h>
+#include <stdio.h>
 #include "syscall.h"
 
 int getrlimit(int resource, struct rlimit *rlim)
 {
-	errno = EOPNOTSUPP;
-	return -1;
+	// TODO Bunnix
+	*rlim = (struct rlimit){
+		.rlim_cur = 4096,
+		.rlim_max = 4096,
+	};
+	return 0;
 }
