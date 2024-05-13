ad5ec2f0d11a0ab413dce64eb98ddf8a1ea0cc5c
Drew DeVault <sir@cmpwn.com>
2024-05-14 00:15:48 +0200

    libc: add tmpfile

---
+++ lib/libc/src/stdio/tmpfile.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "stdio_impl.h"
+
+#define MAXTRIES 100
+
+FILE *tmpfile(void)
+{
+	char s[] = "/tmp/tmpfile_XXXXXX";
+	int fd;
+	FILE *f;
+	int try;
+	for (try=0; try<MAXTRIES; try++) {
+		__randname(s+13);
+		fd = open(s, O_RDWR|O_CREAT|O_EXCL, 0600);
+		if (fd >= 0) {
+			unlinkat(AT_FDCWD, s, 0);
+			f = __fdopen(fd, "w+");
+			if (!f) close(fd);
+			return f;
+		}
+	}
+	return 0;
+}
