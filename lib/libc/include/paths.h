14a13652e073e6b7636989dc028d53d4c9b07e61
Drew DeVault <sir@cmpwn.com>
2024-05-14 12:24:38 +0200

    libc: add paths.h

---
+++ lib/libc/include/paths.h
@@ -0,0 +1,24 @@
+#ifndef _PATHS_H
+#define _PATHS_H
+
+#define	_PATH_DEFPATH "/usr/local/bin:/bin:/usr/bin"
+#define	_PATH_STDPATH "/bin:/usr/bin:/sbin:/usr/sbin"
+
+#define	_PATH_BSHELL	"/bin/sh"
+#define	_PATH_CONSOLE	"/dev/cons"
+#define	_PATH_DEVNULL	"/dev/null"
+#define	_PATH_MAN	"/usr/share/man"
+#define	_PATH_MNTTAB	"/etc/fstab"
+#define	_PATH_NOLOGIN	"/sbin/nologin"
+#define	_PATH_SHADOW	"/etc/shadow"
+#define	_PATH_TTY	"/dev/tty"
+#define	_PATH_VI	"/bin/vi"
+#define _PATH_WTMP	"/dev/null/wtmp"
+
+#define	_PATH_DEV	"/dev/"
+#define	_PATH_TMP	"/tmp/"
+#define	_PATH_VARDB	"/var/lib/misc/"
+#define	_PATH_VARRUN	"/var/run/"
+#define	_PATH_VARTMP	"/var/tmp/"
+
+#endif
