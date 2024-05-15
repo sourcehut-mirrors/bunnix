759a4ca58dbcf4530b8720192fb157f5a5266fdf
Drew DeVault <sir@cmpwn.com>
2024-05-15 15:47:37 +0200

    libc: fix remove(3)

--- lib/libc/src/stdio/remove.c
+++ lib/libc/src/stdio/remove.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include "syscall.h"
 
 int remove(const char *path)
 {
-	int r = __syscall(SYS_unlinkat, AT_FDCWD, path, 0);
-	if (r==-EISDIR) r = __syscall(SYS_unlinkat, AT_FDCWD, path, AT_REMOVEDIR);
-	return __syscall_ret(r);
+	int r = unlinkat(AT_FDCWD, path, 0);
+	if (r < 0 && errno == EISDIR) {
+		r = unlinkat(AT_FDCWD, path, AT_REMOVEDIR);
+	};
+	return r;
 }
