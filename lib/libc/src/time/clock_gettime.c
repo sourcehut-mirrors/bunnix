b7521e725f68f2454d8113bcd03baf56f36fa3b0
Drew DeVault <sir@cmpwn.com>
2024-05-15 10:10:58 +0200

    libc: rig up clock_gettime

--- lib/libc/src/time/clock_gettime.c
+++ lib/libc/src/time/clock_gettime.c
@@ -1,16 +1,11 @@
 #include <time.h>
-#include <errno.h>
 #include <stdint.h>
-#include <stdio.h>
 #include "syscall.h"
 #include "atomic.h"
 
 int __clock_gettime(clockid_t clk, struct timespec *ts)
 {
-	fprintf(stderr, "clock_gettime: ENOSYS\n");
-	// TODO Bunnix
-	errno = ENOSYS;
-	return -1;
+	return syscall(SYS_gettime, clk, ts);
 }
 
 weak_alias(__clock_gettime, clock_gettime);
