5092add226b5537149db384f50598d46a76c3938
Drew DeVault <sir@cmpwn.com>
2024-05-15 12:03:08 +0200

    libc: don't ENOSYS on readlink

--- lib/libc/src/unistd/readlink.c
+++ lib/libc/src/unistd/readlink.c
@@ -1,11 +1,16 @@
+#include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
 #include "syscall.h"
 
 ssize_t readlink(const char *restrict path, char *restrict buf, size_t bufsize)
 {
-	fprintf(stderr, "readlink: ENOSYS\n");
-	errno = ENOSYS;
+	struct stat st;
+	int r = stat(path, &st);
+	if (r != 0) {
+		return r;
+	};
+
+	// Bunnix does not support symlinks
+	errno = EINVAL;
 	return -1;
 }
